dee19764aa258b09ed4798927e6ad710debc0d8f
Adelina <70908927+Adelina122@users.noreply.github.com>
2023-07-30 18:42:19 +0200

    Add current date

--- index.js
+++ index.js
@@ -8,11 +8,23 @@ app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
-    res.render("./index.ejs");
+    var currentDay = new Date().getDay();
+    var currentDate = new Date().getDate();
+    var currentMonth = new Date().getMonth();
+    res.render("./index.ejs", {
+        day: dayNames[currentDay],
+        date: currentDate,
+        month: monthNames[currentMonth],
+    });
+});
+
+app.get("/work", (req, res) => {
+    res.render("./work.ejs");
 });
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
 });
 
-
+const dayNames = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+const monthNames = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "December"];
