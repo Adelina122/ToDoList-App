b4a47058f7498b10157f8da5ec439cfe23639708
Adelina <70908927+Adelina122@users.noreply.github.com>
2023-08-04 23:04:54 +0200

    Update workList: save and delete from MongoDB

--- index.js
+++ index.js
@@ -14,25 +14,39 @@ mongoose.connect("mongodb://127.0.0.1:27017/ToDoListDB");
 
 //Create new schema
 const itemsSchema = new mongoose.Schema({
+    listName: String,
     name: String
 });
 
 //Add some test default models
 const Task = mongoose.model("task", itemsSchema);
 
-const test = new Task({
-    name: "Test"
-});
+// const test = new Task({
+//     name: "Test"
+// });
 
-const defaultTasks = [test];
+// const test2 = new Task({
+//     name: "test2"
+// });
+
+// const defaultTasks = [test, test2];
 
 //Insert new model to the collection
 // await Task.insertMany(defaultTasks).then(console.log("Insert Successfully!"));
+
+// const listSchema = {
+//     name: String,
+//     items: [itemsSchema]
+// };
+
+// const List = mongoose.model("List", listSchema);
+
 
 
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+
 
 app.get("/", async (req, res) => {
 
@@ -40,10 +54,18 @@ app.get("/", async (req, res) => {
     var currentDate = new Date().getDate();
     var currentMonth = new Date().getMonth();
 
-    const usersTasks = await Task.find({});
+
+    //Get all the existing tasks
+    const usersTasks = await Task.find({listName: "Today"});
 
     if (usersTasks == 0) {
-        await Task.insertMany(defaultTasks).then(console.log("Insert Successfully!"));
+    //     //Insert new model to the collection
+        const defaultTask = new Task({
+            listName: "Today",
+            name: "Welcome to your ToDoList"
+        });
+    defaultTask.save();
+    //     await Task.insertMany(defaultTasks).then(console.log("Insert Successfully!"));
         console.log(usersTasks, "Default tasks successfully inserted!");
         return res.redirect("/");
     }
@@ -59,10 +81,12 @@ app.get("/", async (req, res) => {
 
 app.post("/submitMain", (req, res) => {
     const userTask = req.body["todayList"];
+    const listName = "Today";
     // dailyTask.push(newTask);
     // console.log(newTask, dailyTask);
 
     const task = new Task({
+        listName: listName,
         name: userTask
     });
     task.save();
@@ -75,17 +99,38 @@ app.post("/deleteMain", async (req, res) => {
     res.redirect("/");
 });
 
-app.get("/work", (req, res) => {
-    res.render("./work.ejs", { tasksList: workTask});
+// app.get("/:userParams", (req, res) => {
+//     const userParams = req.params.userParams;
+
+//     const list = new List({
+//         name: userParams,
+//         items: defaultTasks
+//     })
+// })
+
+app.get("/work", async (req, res) => {
+    const workTasks = await Task.find({listName: "work"});
+    res.render("./work.ejs", { tasksList: workTasks});
 });
 
 app.post("/submitWork", (req, res) => {
-
-    newTask = req.body["workList"];
-    workTask.push(newTask);
+    const newTask = req.body["workList"];
+    const listName = "work";
+    // workTask.push(newTask);
+    const workTask = new Task({
+        listName: listName,
+        name: newTask
+    });
+    workTask.save();
 
     res.redirect("/work");
 });
+
+app.post("/deleteWork", async (req, res) => {
+    const checkedTaskID = req.body.checkbox;
+    await Task.findByIdAndRemove(checkedTaskID).then(console.log("Successfully deleted checked task"));
+    res.redirect("/work");
+})
 
 app.listen(port, () => {
     console.log(`Listening on port ${port}`);
